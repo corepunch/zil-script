d12425b6bc687c9240a52f05660b7521714fe168
Igor Chernakov <igor.chernakov@gmail.com>
2025-11-08 13:40:20 +0100

    Initial Commit

---
+++ zil-engine/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  zil-engine
+//
+//  Created by Igor Chernakov on 08.11.25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return EXIT_SUCCESS;
+}
